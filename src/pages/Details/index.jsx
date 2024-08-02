ba8950e1bd45015a9f7e965b164c7bf87ac76be9
henriqueAlgauer <algauer.henrique@gmail.com>
2024-08-02 00:19:54 -0300

    alterado estrutura de pastas do projeto

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export function Details() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>Henrique Machado Algauer</p>
+      <span>Vulgo `Piá`</span>
+    </>
+  );
+}
