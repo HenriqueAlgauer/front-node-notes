011891b65f91d571047baf98fafe18fa14acce0b
Henrique <algauer.henrique@gmail.com>
2024-08-02 15:17:51 -0300

    adicionado Theme Provider

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,11 @@
-import "./styles.css";
+import { Container } from "./styles.js";
 
 export function Details() {
   return (
-    <>
+    <Container>
       <h1>Hello World</h1>
       <p>Henrique Machado Algauer</p>
       <span>Vulgo `Piá`</span>
-    </>
+    </Container>
   );
 }
