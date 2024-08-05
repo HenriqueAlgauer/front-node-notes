4004ef567012d8b10c8767e449e699790e5c0522
henriqueAlgauer <algauer.henrique@gmail.com>
2024-08-04 23:07:55 -0300

    componente section pronto

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,12 +1,27 @@
-import { Container } from "./styles.js";
+import { Container, Links } from "./styles.js";
 
 import { Button } from "../../components/Button/index.jsx";
 import { Header } from "../../components/Header/index.jsx";
+import { Section } from "../../components/Section/index.jsx";
 
 export function Details() {
   return (
     <Container>
       <Header />
+
+      <Section title="Links úteis">
+        <Links>
+          <li>
+            <a href="#">https://dev.algauer.com</a>
+          </li>
+          <li>
+            <a href="#">https://dev.algauer.com</a>
+          </li>
+        </Links>
+      </Section>
+
+      <Section title="Marcadores"></Section>
+
       <Button title="Voltar" />
     </Container>
   );
