5cb602236529b29850f9694a01aed58326462c21
Henrique <algauer.henrique@gmail.com>
2024-08-06 17:56:59 -0300

    .

---
+++ src/components/Input/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND900};
+  color: ${({ theme }) => theme.COLORS.GRAY300};
+`;
