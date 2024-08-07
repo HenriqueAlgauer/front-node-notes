d36e89acb855e40a715d053d94dd11831f35a4dc
henriqueAlgauer <algauer.henrique@gmail.com>
2024-08-06 23:40:48 -0300

    componente input finalizado

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -7,4 +7,25 @@ export const Container = styled.div`
 
   background-color: ${({ theme }) => theme.COLORS.BACKGROUND900};
   color: ${({ theme }) => theme.COLORS.GRAY300};
+
+  margin-bottom: 8px;
+  border-radius: 10px;
+
+  > input {
+    height: 56px;
+    width: 100%;
+
+    padding: 12px;
+
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    background: transparent;
+    border: 0;
+
+    &:placeholder {
+      color: ${({ theme }) => theme.COLORS.GRAY300};
+    }
+  }
+  > svg {
+    margin-left: 16px;
+  }
 `;
