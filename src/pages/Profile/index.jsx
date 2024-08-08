64ece48a8fbd1943474c0c7b6c3bde70c470c1ae
Henrique <algauer.henrique@gmail.com>
2024-08-08 13:23:02 -0300

    página Profile finalizada

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,26 @@
+import { FiArrowLeft, FiUser, FiLock, FiMail } from "react-icons/fi";
+import { Container, Form } from "./styles";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+export function Profile() {
+  return (
+    <Container>
+      <header>
+        <a href="#">
+          <FiArrowLeft />
+        </a>
+      </header>
+
+      <Form>
+        <Input placeholder="Nome" type="text" icon={FiUser} />
+        <Input placeholder="Email" type="text" icon={FiMail} />
+        <Input placeholder="Senha atual" type="password" icon={FiLock} />
+        <Input placeholder="Nova senha" type="password" icon={FiLock} />
+
+        <Button title="Salvar" />
+      </Form>
+    </Container>
+  );
+}
