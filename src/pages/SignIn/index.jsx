10ce2a2ad0aefa4e706d19a3271f0e31aa89c2fe
henriqueAlgauer <algauer.henrique@gmail.com>
2024-08-08 00:42:54 -0300

    formulário página SignIn pronto

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,5 +1,25 @@
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
+import { FiMail, FiLock } from "react-icons/fi";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
 
 export function SignIn() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Form>
+        <h1>Notes</h1>
+        <p>Aplicação para salvar e gerenciar sues links úteis</p>
+
+        <h2>Login</h2>
+
+        <Input type="text" placeholder="Email" icon={FiMail} />
+        <Input type="password" placeholder="Senha" icon={FiLock} />
+
+        <Button title={"Entrar"} />
+
+        <a href="#">Criar conta</a>
+      </Form>
+    </Container>
+  );
 }
