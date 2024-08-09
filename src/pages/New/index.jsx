817168b38886a7ecd018aad40aa03ee4fb111280
henriqueAlgauer <algauer.henrique@gmail.com>
2024-08-08 23:58:21 -0300

    Página New finalizada

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,6 +1,7 @@
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
 import { Textarea } from "../../components/Textarea";
+import { Button } from "../../components/Button";
 import { NoteItem } from "../../components/NoteItem";
 import { Section } from "../../components/Section";
 
@@ -25,6 +26,15 @@ export function New() {
             <NoteItem value="https://dev.algauer.com" />
             <NoteItem isNew placeholder="Novo link" />
           </Section>
+
+          <Section title="Marcadores">
+            <div className="tags">
+              <NoteItem value="https://dev.algauer.com" />
+              <NoteItem isNew placeholder="Novo link" />
+            </div>
+          </Section>
+
+          <Button title="Salvar" />
         </Form>
       </main>
     </Container>
