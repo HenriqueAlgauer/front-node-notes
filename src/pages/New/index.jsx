67b195166579dc54f8d7c2f251f7b44b55636ca6
Henrique <algauer.henrique@gmail.com>
2024-08-29 17:54:52 -0300

    update links

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
@@ -10,6 +11,14 @@ import { Section } from "../../components/Section";
 import { Container, Form } from "./styles";
 
 export function New() {
+  const [links, setLinks] = useState([]);
+  const [newLink, setNewLink] = useState("");
+
+  function handleAddLink() {
+    setLinks((prevState) => [...prevState, newLink]);
+    setNewLink("");
+  }
+
   return (
     <Container>
       <Header />
@@ -25,14 +34,22 @@ export function New() {
           <Textarea placeholder={"Observações"} />
 
           <Section title="Links úteis">
-            <NoteItem value="https://dev.algauer.com" />
-            <NoteItem isNew placeholder="Novo link" />
+            {links.map((link, index) => {
+              <NoteItem key={String(index)} value={link} onClick={() => {}} />;
+            })}
+            <NoteItem
+              isNew
+              placeholder="Novo link"
+              value={newLink}
+              onChange={(e) => setNewLink(e.target.value)}
+              onClick={handleAddLink}
+            />
           </Section>
 
           <Section title="Marcadores">
             <div className="tags">
-              <NoteItem value="https://dev.algauer.com" />
-              <NoteItem isNew placeholder="Novo link" />
+              <NoteItem value="react" />
+              <NoteItem isNew placeholder="Nova Tag" />
             </div>
           </Section>
 
