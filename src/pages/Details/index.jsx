08213ebdd046a2db1d0cd6468f381d14d4131344
Pia <algauer.henrique@gmail.com>
2024-09-01 02:13:35 -0300

    retornando os detalhes da nota com a api

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,6 @@
+import { useState, useEffect } from "react";
 import { Container, Links, Content } from "./styles.js";
+import { useParams } from "react-router-dom";
 
 import { Button } from "../../components/Button/index.jsx";
 import { Header } from "../../components/Header/index.jsx";
@@ -6,40 +8,53 @@ import { Section } from "../../components/Section/index.jsx";
 import { Tag } from "../../components/Tag/index.jsx";
 import { ButtonText } from "../../components/ButtonText/index.jsx";
 
+import { api } from "../../services/api.js";
+
 export function Details() {
+  const [data, setData] = useState(null);
+  const params = useParams();
+
+  useEffect(() => {
+    async function fetchNote() {
+      const response = await api.get(`/notes/${params.id}`);
+      setData(response.data);
+    }
+
+    fetchNote();
+  }, []);
+
   return (
     <Container>
       <Header />
-      <main>
-        <Content>
-          <ButtonText title="Excuir nota" />
+      {data && (
+        <main>
+          <Content>
+            <ButtonText title="Excuir nota" />
 
-          <h1>Introdução ao React</h1>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Dignissimos, ullam alias dolor aliquid assumenda et nihil incidunt!
-            Voluptates pariatur mollitia, aspernatur nesciunt officiis, adipisci
-            nostrum architecto, repellendus sunt consectetur officia.
-          </p>
-          <Section title="Links úteis">
-            <Links>
-              <li>
-                <a href="#">https://dev.algauer.com</a>
-              </li>
-              <li>
-                <a href="#">https://dev.algauer.com</a>
-              </li>
-            </Links>
-          </Section>
-
-          <Section title="Marcadores">
-            <Tag title="express" />
-            <Tag title="node" />
-          </Section>
-
-          <Button title="Voltar" />
-        </Content>
-      </main>
+            <h1>{data.title}</h1>
+            <p>{data.description}</p>
+            {data.links && (
+              <Section title="Links úteis">
+                <Links>
+                  {data.links.map((link) => (
+                    <li key={String(link.id)}>
+                      <a href={link.url}>{link.url}</a>
+                    </li>
+                  ))}
+                </Links>
+              </Section>
+            )}
+            {data.tags && (
+              <Section title="Marcadores">
+                {data.tags.map((tag) => (
+                  <Tag key={String(tag.id)} title={tag.name} />
+                ))}
+              </Section>
+            )}
+            <Button title="Voltar" />
+          </Content>
+        </main>
+      )}
     </Container>
   );
 }
